ca66e6cf00ba54a0b08a3f82830f3786caeea8b8
TadewosBell <bell6@umbc.edu>
2019-09-29 17:16:29 -0400

    added ajax client call

---
+++ LifeSaver/src/Client/LifeSaverClient.jsx
@@ -0,0 +1,48 @@
+const LIFESAVER_ENDPOINTS = {
+    DEV_ENV: 'http://localhost:5000/',
+    PROD_ENV: '',
+  };
+  
+const CURRENT_ENDPOINT = LIFESAVER_ENDPOINTS.DEV_ENV;
+export function performHTTPRequest(method, path, jsonData, onSuccess, onFailure) {
+const endpoint = CURRENT_ENDPOINT;
+const url = endpoint + path;
+if (jsonData != null) {
+    var body = JSON.stringify(jsonData);
+}
+fetch(url, {
+    method: method,
+    cache: 'no-cache',
+    headers: {
+    'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrer: 'no-referrer',
+    body: body,
+}).then((res) => {
+    if (onSuccess) {
+    res.json().then((body) => {
+        onSuccess(body);
+    });
+    }
+}).catch((err) => {
+    if (onFailure) {
+    onFailure(err.toString());
+    }
+});
+}
+
+
+export function getJson(token, onSuccess, onFailure){
+    const postData = {
+        token,
+    }
+
+    performHTTPRequest('POST', 'JsonObject', postData, onSuccess, onFailure);
+}
+
+export function getUserName(userName, onSuccess, onFailure) {
+
+    const getString = `GetRequest/${userName}`;
+    performHTTPRequest('GET', getString, null, onSuccess, onFailure);
+  }
--- LifeSaver/src/Dashboard/DashBoard.jsx
+++ LifeSaver/src/Dashboard/DashBoard.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from '../../node_modules/react';
+import { getJson, getUserName } from '../Client/LifeSaverClient';
 
 class DashBoard extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            userName: "",
+         }
     }
+
+    componentDidMount() {
+        const onSuccess = (res) => {
+            console.log(res);
+            const userName = res['userName'];
+            this.setState({
+                userName,
+            })
+        }
+
+        const onFailure = (res) => {
+
+        }
+
+        getUserName('TadewosBell',onSuccess, onFailure);
+    }
+
     render() { 
+        const { userName } = this.state;
         return ( 
             <div>
-                <h1>Hello</h1>
+                <h1>Hello: {userName}</h1>
             </div>
          );
     }
