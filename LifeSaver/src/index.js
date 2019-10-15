20639849660ebdc9851b940cb8359fac42d61189
TheUltimateAbsol <thebellis618@gmail.com>
2019-10-15 16:16:51 -0400

    Undid changes to index.js

--- LifeSaver/src/index.js
+++ LifeSaver/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CallForm from './CallDesk/CallForm';
+import HomePage from './Home/Home';
 
 ReactDOM.render(
-  <CallForm />,
+  <HomePage />,
   document.getElementById('root')
 );
