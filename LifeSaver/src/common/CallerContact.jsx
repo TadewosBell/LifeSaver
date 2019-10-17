a4edcfb13503940a17da2c4d3b770d3b97eae972
TheUltimateAbsol <thebellis618@gmail.com>
2019-10-16 23:14:12 -0400

    Added Call Desk Call Form (#5)
    * Implemented most of form, except for address
    
    * Stole Shea's common files
    
    * Fixed the style a bit
    
    Tweaked the CSS of the form to be a bit cleaner
    
    * Fixed margin of map display
    
    Added a margin to the map Box element
    
    * Undid changes to index.js
    
    * Removed debug tag
    
    * Cleaned up the branch and added a test submit function
    
    * Added progress bar
    
    * Tweaked the Callfrom class and LifesaverClient classes to interact better with the server.
    
    * Everything works now

---
+++ LifeSaver/src/common/CallerContact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles, Typography, Card, CardContent } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    card: {
+    }
+});
+
+export default function CallerContact(props) {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography variant="h5" gutterBottom>Caller</Typography>
+                <Typography variant="body1" gutterBottom><b>Phone: </b>{props.phoneNumber}</Typography>
+            </CardContent>
+        </Card>
+    );
+}
