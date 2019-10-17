0abfcc1512af9b9c112f794ddc10eb1862cdf844
ssandif <ssandifer@umbc.edu>
2019-10-16 21:15:30 -0400

    Add async request() method

--- LifeSaver/src/Client/LifeSaverClient.jsx
+++ LifeSaver/src/Client/LifeSaverClient.jsx
@@ -4,32 +4,59 @@ const LIFESAVER_ENDPOINTS = {
   };
   
 const CURRENT_ENDPOINT = LIFESAVER_ENDPOINTS.DEV_ENV;
+
 export function performHTTPRequest(method, path, jsonData, onSuccess, onFailure) {
-const endpoint = CURRENT_ENDPOINT;
-const url = endpoint + path;
-if (jsonData != null) {
-    var body = JSON.stringify(jsonData);
-}
-fetch(url, {
-    method: method,
-    cache: 'no-cache',
-    headers: {
-    'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: body,
-}).then((res) => {
-    if (onSuccess) {
-    res.json().then((body) => {
-        onSuccess(body);
+    const endpoint = CURRENT_ENDPOINT;
+    const url = endpoint + path;
+    if (jsonData != null) {
+        var body = JSON.stringify(jsonData);
+    }
+    fetch(url, {
+        method: method,
+        cache: 'no-cache',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: body,
+    }).then((res) => {
+        if (onSuccess) {
+        res.json().then((body) => {
+            onSuccess(body);
+        });
+        }
+    }).catch((err) => {
+        if (onFailure) {
+        onFailure(err.toString());
+        }
     });
+}
+
+async function request(method, path, jsonData) {
+    const endpoint = CURRENT_ENDPOINT;
+    const url = endpoint + path;
+    const requestInfo = {
+        method: method,
+        cache: 'no-cache',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
     }
-}).catch((err) => {
-    if (onFailure) {
-    onFailure(err.toString());
+
+    if (jsonData != null) {
+        requestInfo.body = JSON.stringify(jsonData);
     }
-});
+
+    const response = await fetch(url, requestInfo);
+
+    return await response.json();
+}
+
+export function getMissions() {
+    return request('GET', 'Missions');
 }
 
 
