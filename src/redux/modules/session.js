7aa85dd1d06977e5b7fb152af68cbf9b1afbce99
Supreme Overlord of the Universe <16561590+MNchino@users.noreply.github.com>
2019-11-21 15:39:06 -0500

    Added small thingy

--- src/redux/modules/session.js
+++ src/redux/modules/session.js
@@ -1,28 +1,27 @@
 const LOG_IN = 'LifeSaver/session/LOG_IN';
 const LOG_OUT = 'LifeSaver/session/LOG_OUT';
+const UPDATE_TOKEN = 'LifeSaver/session/UPDATE_TOKEN';
 
-export const loginsession = (email, message) => ({ type: LOG_IN, email, message });
 
-export const logoutsession = (email) => ({ type: LOG_OUT, email });
+export const loginsession = (email, password) => ({ type: LOG_IN, email, password });
+
+export const logoutsession = () => ({ type: LOG_OUT });
+
+export const updatetokensession = (token) => ({ type: UPDATE_TOKEN, token });
+
 
 const session = (state = {}, action) => {
   switch (action.type) {
-    case LOG_IN:
+    case UPDATE_TOKEN:
       return {
         ...state,
-        session: {
-          ...state.session,
-          loggedIn: true
-        }
+        token: action.token
       };
     case LOG_OUT:
-    return {
+      return {
         ...state,
-        session: {
-        ...state.session,
-        loggedIn: true
-        }
-    };
+        token: null
+      };
     default:
       return state;
   }
