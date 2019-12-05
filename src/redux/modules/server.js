56aa3b0674393aa1f80aa4445e86859c5e3726e7
ssandif <ssandifer@umbc.edu>
2019-12-05 16:45:28 -0500

    Add users to mission

--- src/redux/modules/server.js
+++ src/redux/modules/server.js
@@ -3,10 +3,14 @@ import { map, mergeMap, delay, combineAll, flatMap } from 'rxjs/operators'
 import { ofType } from 'redux-observable'
 import { 
     getMissions as getMissionsPromise,
-    getCalls as getCallsPromise ,
+    getCalls as getCallsPromise,
     addCallToMission as addCallToMissionPromise,
     removeCallFromMission as removeCallFromMissionPromise,
-    getCallsForMission as getCallsForMissionPromise
+    getCallsForMission as getCallsForMissionPromise,
+    getUsers as getUsersPromise,
+    addUserToMission as addUserToMissionPromise,
+    removeUserFromMission as removeUserFromMissionPromise,
+    getUsersForMission as getUsersForMissionPromise
 } from '../../Client/LifeSaverClient';
 
 const GET_MISSIONS = 'LifeSaver/server/GET_MISSIONS';
@@ -14,9 +18,15 @@ const UPDATE_MISSIONS = 'LifeSaver/server/UPDATE_MISSIONS';
 const GET_UNASSIGNED_CALLS = 'LifeSaver/server/GET_UNASSIGNED_CALLS';
 const UPDATE_UNASSIGNED_CALLS = 'LifeSaver/server/UPDATE_UNASSIGNED_CALLS';
 const GET_CALLS_FOR_MISSION = 'LifeSaver/server/GET_CALLS_FOR_MISSION';
-const UPDATE_CURRENT_MISSION = 'LifeSaver/server/UPDATE_CURRENT_MISSION';
+const UPDATE_CALLS = 'LifeSaver/server/UPDATE_CURRENT_MISSION';
 const ADD_CALL_TO_MISSION = 'LifeSaver/server/ADD_CALL_TO_MISSION';
 const REMOVE_CALL_FROM_MISSION = 'LifeSaver/server/REMOVE_CALL_FROM_MISSION';
+const GET_UNASSIGNED_USERS = 'LifeSaver/server/GET_UNASSIGNED_USERS';
+const UPDATE_UNASSIGNED_USERS = 'LifeSaver/server/UPDATE_UNASSIGNED_USERS';
+const GET_USERS_FOR_MISSION = 'LifeSaver/server/GET_CALLS_FOR_MISSION';
+const UPDATE_USERS = 'LifeSaver/server/UPDATE_USERS';
+const ADD_USER_TO_MISSION = 'LifeSaver/server/ADD_USER_TO_MISSION';
+const REMOVE_USER_FROM_MISSION = 'LifeSaver/server/REMOVE_USER_FROM_MISSION';
 
 
 export const getMissions = () => ({ type: GET_MISSIONS });
@@ -24,9 +34,15 @@ export const updateMissions = (missions) => ({ type: UPDATE_MISSIONS, missions }
 export const getUnassignedCalls = () => ({ type: GET_UNASSIGNED_CALLS });
 export const updateUnassignedCalls = (unassignedCalls) => ({ type: UPDATE_UNASSIGNED_CALLS, unassignedCalls });
 export const getCallsForMission = (mission) => ({ type: GET_CALLS_FOR_MISSION, mission });
-export const updateCurrentMission = (mission, calls) => ({ type: UPDATE_CURRENT_MISSION, mission, calls });
+export const updateCalls = (mission, calls) => ({ type: UPDATE_CALLS, mission, calls });
 export const addCallToMission = (mission, call) => ({ type: ADD_CALL_TO_MISSION, mission, call });
 export const removeCallFromMission = (mission, call) => ({ type: REMOVE_CALL_FROM_MISSION, mission, call });
+export const getUnassignedUsers = () => ({ type: GET_UNASSIGNED_USERS });
+export const updateUnassignedUsers = (unassignedUsers) => ({ type: UPDATE_UNASSIGNED_USERS, unassignedCalls: unassignedUsers });
+export const getUsersForMission = (mission) => ({ type: GET_USERS_FOR_MISSION, mission });
+export const updateUsers = (mission, users) => ({ type: UPDATE_USERS, mission, calls: users });
+export const addUserToMission = (mission, user) => ({ type: ADD_USER_TO_MISSION, mission, call: user });
+export const removeUserFromMission = (mission, user) => ({ type: REMOVE_USER_FROM_MISSION, mission, call: user });
 
 export const getMissionsEpic = action$ => action$.pipe(
     ofType(GET_MISSIONS),
@@ -51,7 +67,7 @@ export const getCallsForMissionEpic = action$ => action$.pipe(
   ofType(GET_CALLS_FOR_MISSION),
   mergeMap(action =>
       from(getCallsForMissionPromise(action.mission)).pipe(
-          map(calls => updateCurrentMission(action.mission, calls))
+          map(calls => updateCalls(action.mission, calls))
       )
   )
 );
@@ -60,6 +76,7 @@ export const addCallToMissionEpic = action$ => action$.pipe(
     ofType(ADD_CALL_TO_MISSION),
     mergeMap(action =>
         from(addCallToMissionPromise(action.mission, action.call)).pipe(
+            delay(100),
             map(getUnassignedCalls)
         )
     )
@@ -69,6 +86,45 @@ export const removeCallFromMissionEpic = action$ => action$.pipe(
     ofType(REMOVE_CALL_FROM_MISSION),
     mergeMap(action =>
         from(removeCallFromMissionPromise(action.mission, action.call)).pipe(
+            delay(100),
+            map(getUnassignedCalls)
+        )
+    )
+);
+
+export const getUnassignedUsersEpic = action$ => action$.pipe(
+    ofType(GET_UNASSIGNED_USERS),
+    mergeMap(() =>
+        from(getUsersPromise()).pipe(
+            map(x => x.filter(y => !y.mission)),
+            map(updateUnassignedUsers)
+        )
+    )
+);
+
+export const getUsersForMissionEpic = action$ => action$.pipe(
+    ofType(GET_USERS_FOR_MISSION),
+    mergeMap(action =>
+        from(getUsersForMissionPromise(action.mission)).pipe(
+            map(users => updateUsers(action.mission, users))
+        )
+    )
+);
+
+export const addUserToMissionEpic = action$ => action$.pipe(
+    ofType(ADD_USER_TO_MISSION),
+    mergeMap(action =>
+        from(addUserToMissionPromise(action.mission, action.call)).pipe(
+            delay(100),
+            map(getUnassignedCalls)
+        )
+    )
+);
+
+export const removeUserFromMissionEpic = action$ => action$.pipe(
+    ofType(REMOVE_USER_FROM_MISSION),
+    mergeMap(action =>
+        from(removeUserFromMissionPromise(action.mission, action.user)).pipe(
             delay(100),
             map(getUnassignedCalls)
         )
@@ -87,10 +143,20 @@ const server = (state = {}, action) => {
         ...state,
         unassignedCalls: action.unassignedCalls
       };
-    case UPDATE_CURRENT_MISSION:
+    case UPDATE_UNASSIGNED_USERS:
+        return {
+          ...state,
+          unassignedUsers: action.unassignedUsers
+        };
+    case UPDATE_CALLS:
         return {
             ...state,
             missions: state.missions.map(x => x._id.$oid === action.mission ? {...x, calls: action.calls} : {...x})
+        };
+    case UPDATE_USERS:
+        return {
+            ...state,
+            missions: state.missions.map(x => x._id.$oid === action.mission ? {...x, users: action.users} : {...x})
         };
     default:
       return state;
