a9463015711c964acb27b4935964bddd03924936
Supreme Overlord of the Universe <16561590+MNchino@users.noreply.github.com>
2019-12-10 16:28:55 -0500

    Added another icon

--- src/Login/Register.jsx
+++ src/Login/Register.jsx
@@ -23,6 +23,8 @@ import {ErrorSnackbar, ERROR_SNACKBAR} from "../common/SnackbarTypes"
 import { useDispatch } from 'react-redux'
 import { showSnackbar } from '../redux/modules/snackbar';
 import { SuccessSnackbar, SUCCESS_SNACKBAR } from '../common/SnackbarTypes';
+import icon from "../Login/icon.png"
+
 
 function Copyright() {
   return (
@@ -120,9 +122,8 @@ export default function Register() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
+      <img src={icon} width="72" height="72" />
+
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
